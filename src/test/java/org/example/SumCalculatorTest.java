d3e160663ae710e51324fe6a3044d6a480ddb90c
Світлана <simbratovich@gmail.com>
2024-05-06 17:28:45 +0300

    Initial commit

---
+++ src/test/java/org/example/SumCalculatorTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumCalculatorTest {
+    private SumCalculator sumCalculator;
+
+    @BeforeEach
+    public void setUp() {
+        sumCalculator = new SumCalculator();
+    }
+
+    @Test
+    public void testSumWith1() {
+        assertEquals(1, sumCalculator.sum(1));
+    }
+
+    @Test
+    public void testSumWith3() {
+        assertEquals(6, sumCalculator.sum(3));
+    }
+
+    @Test
+    public void testSumWith0ThrowsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> sumCalculator.sum(0));
+    }
+}
